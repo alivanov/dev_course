a398979b9b60e60ad9a9b5431131e74d64cfad79
alivanov <ivanov@intspirit.com>
2021-01-28 12:36:27 +0300

    add Lesson 4 with examples

---
+++ Block_1/Lesson_4/Lesson_4_1/index.js
@@ -0,0 +1,6 @@
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+  .catch((error) => {
+    console.error('Error:', error);
+  })
