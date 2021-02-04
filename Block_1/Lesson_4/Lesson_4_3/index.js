b976b603610f0ecaba8597127aa87c768f98aca0
alivanov <ivanov@intspirit.com>
2021-02-04 10:50:42 +0300

    Block 1, Lesson 4: add @media examples

--- Block_1/Lesson_4/Lesson_4_3/index.js
+++
@@ -1,6 +0,0 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
-  .catch((error) => {
-    console.error('Error:', error);
-  })
