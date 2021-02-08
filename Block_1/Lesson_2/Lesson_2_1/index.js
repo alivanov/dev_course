b4baeed74faceece5372ca902daad55b8edff19e
alivanov <ivanov@intspirit.com>
2021-02-08 12:09:31 +0300

    fix Block 1 Lesson 3 class names

--- Block_1/Lesson_2/Lesson_2_1/index.js
+++ Block_1/Lesson_2/Lesson_2_1/index.js
@@ -1,6 +1,21 @@
+function foo(n) {
+  console.log(n*2);
+}
+
 fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(response => response.json())
-  .then(json => console.log(json))
+  .then(json => {
+    console.log(json)
+
+    let arr = [1,2,3,4,5];
+    
+    //to discuss debugging in browsers...
+    //debugger
+
+    for (n of arr) {
+      foo(n);
+    }
+  })
   .catch((error) => {
     console.error('Error:', error);
   })
