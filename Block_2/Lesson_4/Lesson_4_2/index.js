89458581758db22d82ffd689d70780ba8d956f02
alivanov <ivanov@intspirit.com>
2021-03-03 12:40:02 +0300

    Block 2 Lesson 4: BigInt & Intl examples

--- Block_2/Lesson_4/Lesson_4_2/index.js
+++ Block_2/Lesson_4/Lesson_4_2/index.js
@@ -40,3 +40,66 @@ function findSingle(arr) {
 }
 
 console.log(findSingle([1,1,2,2,3,3,4,5,4])); //5
+
+//----------------------------
+
+console.log('\x1b[31m', '//BigInt --------------------------------------------- \n', '\x1b[0m');
+
+const fibonacci = (num) => {
+
+  const table = new Array(num + 1).fill(0n);
+
+  table[1] = 1n;
+  table[2] = 1n;
+
+  for(let i = 3; i <= num; i++) {
+    table[i] = BigInt(table[i-1] + table[i-2]);
+  }
+
+  return table[num]
+}
+
+console.log(fibonacci(5000));
+
+//----------------------------
+
+console.log('\x1b[31m', '//Intl --------------------------------------------- \n', '\x1b[0m');
+
+let collator = new Intl.Collator();
+console.log( "ёжик" > "яблоко" ); // true (ёжик is greater, not correct)
+console.log( collator.compare("ёжик", "яблоко") ); // -1 (ёжик is less, correct)
+
+//sensivity
+console.log( collator.compare("ЁжиК", "ёжик") ); // 1, not equal ("ЁжиК" > "ёжик")
+collator = new Intl.Collator(undefined, {sensitivity: "accent"});
+console.log( collator.compare("ЁжиК", "ёжик") ); // 0, equal
+
+//to be executed in a browser----------------------------
+
+let date = new Date(2021, 3, 3, 19, 30, 0);
+
+let formatter = new Intl.DateTimeFormat("ru");
+console.log( formatter.format(date) );
+
+formatter = new Intl.DateTimeFormat("en-US");
+console.log( formatter.format(date) );
+
+//time
+date = new Date(2021, 3, 3, 19, 30, 0);
+
+formatter = new Intl.DateTimeFormat("ru", {
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric"
+});
+
+console.log( formatter.format(date) );
+
+//numbers
+formatter = new Intl.NumberFormat("ru", {
+  style: "currency",
+  currency: "GBP",
+  minimumFractionDigits: 2
+});
+
+console.log( formatter.format(1234.5) );
