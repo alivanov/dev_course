a4f18313c26e6383623f165f202ba47de48cf052
alivanov <ivanov@intspirit.com>
2021-03-09 11:15:58 +0300

    webpack demo: step 2 (minimal webpack config)

--- Block_4/Lesson_1/Lesson_1_3/webpack_demo/src/index.js
+++ Block_4/Lesson_1/Lesson_1_3/webpack_demo/src/index.js
@@ -1,3 +1,10 @@
+/*
+  1. $ npm init - y
+  2. $ npm i -D webpack webpack-cli
+  3. create webpack.config.js file
+  4. $ ./node_modules/.bin/webpack
+*/
+
 const post = new Post('my post');
 
 console.log('post', post.toString());
