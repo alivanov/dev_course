eaef96a5b53ba1138a8fa5dad6fa9396420a8f48
alivanov <ivanov@intspirit.com>
2021-03-23 13:00:00 +0300

    add Block 4 Lesson 2: eslint example

---
+++ Block_4/Lesson_2/Lesson_2_3/src/index.js
@@ -0,0 +1,15 @@
+/*
+1. cd coding-directory
+2. npm init -y
+3. npm i -D eslint eslint-config-airbnb-base eslint-plugin-import
+4. Create .eslintrc: { "extends": "airbnb-base" }
+5. In VS Code, Ctrl + Shift + X
+6. Search ESLint
+7. Install ESLint
+8. Restart VS Code
+*/
+
+const a = 1;
+const b = 2;
+
+console.log(a, b);
