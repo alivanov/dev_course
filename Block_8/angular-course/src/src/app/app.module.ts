abf41833f35b15031eacc41844d1d4d42bb16b00
Zuev Roman <roman.zuev@intspirit.com>
2021-05-13 02:45:20 +0300

    Block 8 Lesson 1 (overview): add template and style files to component

---
+++ Block_8/angular-course/src/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
