eb4330e1c46e1cd3a60e87308e5c47f2e9e41395
Zuev Roman <roman.zuev@intspirit.com>
2021-05-13 02:51:35 +0300

    Block 8 Lesson 1 (overview): add property binding, class binding and todo-item styles

--- Block_8/angular-course/src/app/components/todo-item/todo-item.component.ts
+++ Block_8/angular-course/src/app/components/todo-item/todo-item.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
 
-/**
- * Interpolation
- * @link https://angular.io/guide/interpolation
- */
-
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.scss']
 })
 export class TodoItemComponent {
-  todoTitle = 'Learn Angular';
-
   todoItem = { id: 1, text: 'Learn Angular', status: false };
-
-  getMaxNumber(): number {
-    return Math.max(6, 8, 1, 4, 5);
-  }
 }
