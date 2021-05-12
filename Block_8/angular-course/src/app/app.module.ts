bb4906f21544d6dc28f07dc4568da204045c8a0b
Zuev Roman <roman.zuev@intspirit.com>
2021-05-13 02:43:00 +0300

    Block 8 Lesson 1 (overview): add todo item component

--- Block_8/angular-course/src/app/app.module.ts
+++ Block_8/angular-course/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ Block_8/angular-course/src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  template: '<p>todo-item works!</p>',
+  styles: [
+    'p {color: red; font-size: 33px; margin-top: 15px;}'
+  ]
+})
+export class TodoItemComponent {
+}
