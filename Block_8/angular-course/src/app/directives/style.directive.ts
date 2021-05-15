2d9fc11b1361853cf02104fe062b24d30c395621
Zuev Roman <roman.zuev@intspirit.com>
2021-05-15 21:45:08 +0300

    Block 8 Lesson 3 (directive): add host listener

--- Block_8/angular-course/src/app/directives/style.directive.ts
+++ Block_8/angular-course/src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
@@ -6,5 +6,21 @@ import { Directive, ElementRef, Renderer2 } from '@angular/core';
 export class StyleDirective {
   constructor(private el: ElementRef, private r: Renderer2) {
     this.r.setStyle(this.el.nativeElement, 'color', 'red');
+  }
+
+  @HostListener('mouseover') mouseOver() {
+    this.setShadow();
+  }
+
+  @HostListener('mouseleave') mouseLeave() {
+    this.removeShadow();
+  }
+
+  setShadow() {
+    this.r.setStyle(this.el.nativeElement, 'boxShadow', 'inset 0px 0px 8px 3px rgb(60 60 60 / 25%)');
+  }
+
+  removeShadow() {
+    this.r.setStyle(this.el.nativeElement, 'boxShadow', null);
   }
 }
