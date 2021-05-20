0e17f33bb96de41458a362784325fa5e61732b45
Zuev Roman <roman.zuev@intspirit.com>
2021-05-21 00:47:45 +0300

    Block 8 Lesson 4 (pipe): add async pipe

--- Block_8/angular-course/src/app/app.component.ts
+++ Block_8/angular-course/src/app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ITodo } from './types/interfaces';
 
+const FIVE_SECONDS = 5000;
+const TODOS_MOCK = [
+  { id: 1, text: 'Buy milk', status: false },
+  { id: 2, text: 'Make coffee', status: false },
+  { id: 3, text: 'Smoke a cigarette', status: false },
+  { id: 4, text: 'Learn Angular', status: false }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,52 +17,51 @@ import { ITodo } from './types/interfaces';
 export class AppComponent implements OnInit {
   newTodoText = '';
 
-  todoList: ITodo[] = [
-    { id: 1, text: 'Buy milk', status: false },
-    { id: 2, text: 'Make coffee', status: false },
-    { id: 3, text: 'Smoke a cigarette', status: false },
-    { id: 4, text: 'Learn Angular', status: false },
-  ];
+  todoList: Promise<ITodo[]>;
 
-  // @ViewChild('todoInput', { static: true })
-  // todoInput: ElementRef;
+  @ViewChild('todoInput', { static: true })
+  todoInput: ElementRef;
 
   ngOnInit(): void {
-    // this.focusTodoInput();
+    this.focusTodoInput();
+
+    this.todoList = new Promise<ITodo[]>((resolve) => {
+      setTimeout(() => resolve(TODOS_MOCK), FIVE_SECONDS);
+    });
   }
 
   addTodo(): void {
-    const newTodoText = this.newTodoText.trim();
-
-    if (newTodoText) {
-      const newTodo = this.createTodo(newTodoText);
-      this.todoList.push(newTodo);
-      // this.clearTodoInput();
-    }
+  //   const newTodoText = this.newTodoText.trim();
+  //
+  //   if (newTodoText) {
+  //     const newTodo = this.createTodo(newTodoText);
+  //     this.todoList.push(newTodo);
+  //     this.clearTodoInput();
+  //   }
   }
 
   removeTodo(id: number): void {
-    this.todoList = this.todoList.filter((todo) => todo.id !== id);
+  //   this.todoList = this.todoList.filter((todo) => todo.id !== id);
   }
 
-  private clearTodoInput(): void {
-    this.newTodoText = '';
-  }
+  // private clearTodoInput(): void {
+  //   this.newTodoText = '';
+  // }
 
-  private createTodo(content: string): ITodo {
-    const id = this.findLastId() + 1;
-    return { id, text: content, status: false };
-  }
+  // private createTodo(content: string): ITodo {
+  //   const id = this.findLastId() + 1;
+  //   return { id, text: content, status: false };
+  // }
 
-  private findLastId(): number {
-    if (this.todoList.length) {
-      return Math.max(...this.todoList.map((item) => item.id));
-    }
-
-    return 1;
-  }
+  // private findLastId(): number {
+  //   if (this.todoList.length) {
+  //     return Math.max(...this.todoList.map((item) => item.id));
+  //   }
+  //
+  //   return 1;
+  // }
 
   private focusTodoInput(): void {
-    // this.todoInput.nativeElement.focus();
+    this.todoInput.nativeElement.focus();
   }
 }
