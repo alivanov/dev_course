6af86069c5b769ccbdbc6e28da4b8403256fa258
Zuev Roman <roman.zuev@intspirit.com>
2021-06-28 02:10:18 +0300

    Block 8 Lesson 8 (routing): work with route params

--- Block_8/angular-course/src/app/app-routing.module.ts
+++ Block_8/angular-course/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './post/post.component';
 
 // http://localhost:4200/ -> HomeComponent
 // http://localhost:4200/about -> AboutComponent
@@ -11,7 +12,8 @@ import { PostsComponent } from './posts/posts.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'posts', component: PostsComponent }
+  { path: 'posts', component: PostsComponent },
+  { path: 'posts/:id', component: PostComponent },
 ];
 
 @NgModule({
