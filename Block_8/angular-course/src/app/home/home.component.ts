405b2fbb3e013eaeeaf0dde2e8358038defccbfb
Zuev Roman <roman.zuev@intspirit.com>
2021-06-28 01:47:16 +0300

    Block 8 Lesson 9 (routing): add program navigation

--- Block_8/angular-course/src/app/home/home.component.ts
+++ Block_8/angular-course/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  goToPostsPage(): void {
+    this.router.navigate(['/posts']);
+  }
 }
