3ea0a4f87d337eaced8bf7a639f1581562af5425
Zuev Roman <roman.zuev@intspirit.com>
2021-06-28 02:20:15 +0300

    Block 8 Lesson 8 (routing): add redirect to error page

--- Block_8/angular-course/src/app/app-routing.module.ts
+++ Block_8/angular-course/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AboutComponent } from './about/about.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostComponent } from './post/post.component';
 import { AboutExtraComponent } from './about-extra/about-extra.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 // http://localhost:4200/ -> HomeComponent
 // http://localhost:4200/about -> AboutComponent
@@ -19,6 +20,8 @@ const routes: Routes = [
   },
   { path: 'posts', component: PostsComponent },
   { path: 'posts/:id', component: PostComponent },
+  { path: 'error', component: ErrorPageComponent },
+  { path: '**', redirectTo: '/error' }
 ];
 
 @NgModule({
